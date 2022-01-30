85b8a2788ebf84b8962a13530e61eb93b001b371
Cesar Torres <torres-cesar@outlook.com>
2022-01-30 12:54:44 -0600

    Style div container

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function createContainer() {
     let container = document.createElement("div");
     document.body.append(container);
-
+    
     function styleContainer() {
-        container.classList.add("container");
+        container.style.width = "640px";
+        container.style.height = "640px";
+        container.style.border = "4px solid black";
     }
     styleContainer();
 }
