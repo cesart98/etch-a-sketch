d8eab9ec2db8144a5d1e8f603cf327c9284c68c6
Cesar Torres <torres-cesar@outlook.com>
2022-01-30 12:35:30 -0600

    Add class name "container" to container element

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 function createContainer() {
     let container = document.createElement("div");
     document.body.append(container);
+
+    function styleContainer() {
+        container.classList.add("container");
+    }
+    styleContainer();
 }
 
 createContainer();
