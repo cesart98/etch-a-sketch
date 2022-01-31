880f6535edcdf38b27fa61b5b8e65985d83347c6
Cesar Torres <torres-cesar@outlook.com>
2022-01-31 13:51:42 -0600

    Migrate style declarations to style.css

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 function createContainer() {
     let container = document.createElement("div");
+    container.classList.add("container");
     document.body.append(container);
     
-    function styleContainer() {
-        container.style.width = "640px";
-        container.style.height = "640px";
-        container.style.border = "4px solid black";
-    }
-    styleContainer();
+    
+}
+
+function createGrid() {
+    let gridBox = document.createElement("div");
+    document.body.div.append(gridBox);
 }
 
 createContainer();
