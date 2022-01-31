c37aca565cca63db734074a362a43afb0b70486e
Cesar Torres <torres-cesar@outlook.com>
2022-01-31 14:58:53 -0600

    Add 16x16 grid inside container

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-function createContainer() {
-    let container = document.createElement("div");
-    container.classList.add("container");
-    document.body.append(container);
-    
-    
-}
-
 function createGrid() {
-    let gridBox = document.createElement("div");
-    document.body.div.append(gridBox);
+    function addContainer() {
+        let container = document.createElement("div");
+        container.classList.add("container");
+        document.body.append(container);
+    }
+    function addGridSquare() {
+        let container = document.querySelector(".container");
+        let gridSquare = document.createElement("div");   
+        gridSquare.classList.add("grid-square");
+        container.append(gridSquare);
+    }
+
+    addContainer();
+    for (let i = 1; i < 257; i++) {
+        addGridSquare();
+    }
 }
 
-createContainer();
+createGrid();
